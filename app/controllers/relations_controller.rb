92914db7cb1fa1f6f1a37b0ccb6f43bc7b53350b
akaoni <95534300+akaoni0000@users.noreply.github.com>
2024-02-29 21:17:14 +0900

    Update relations_controller.rb

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -7,6 +7,8 @@ class RelationsController < ApplicationController
 
         if params[:page_follow].present? #フォローフォロワー画面ではなく名前の横を押した時
             @page_follow = true
+        else
+            @page_follow = false
         end
     end
 
@@ -17,6 +19,8 @@ class RelationsController < ApplicationController
 
         if params[:page_follow].present?
             @page_follow = true
+        else
+            @page_follow = false
         end
     end
 
